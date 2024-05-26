137ab9991dea72327db132ad5aef1341a84917c5
youssef emad eldin <youssefemadeldn>
2024-05-26 19:12:39 +0300

    create Cubit

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:basketball/cubit/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit(super.initialState);
+}
+
+
+/*
+input: Function
+                Cubit
+output State
+*/
