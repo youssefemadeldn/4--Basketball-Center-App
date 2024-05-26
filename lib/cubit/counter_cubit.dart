6c2b6e75252cea90b191fa2a2c9907a3135349df
youssef emad eldin <youssefemadeldn>
2024-05-26 19:32:12 +0300

    add Cubit function "Logic"

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -3,9 +3,25 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit(super.initialState);
+  int teamAPoints = 0;
+
+  int teamBPoints = 0;
+
+  void increment({required String teamChar, required int buttonNumber}) {
+    if (teamChar == 'A') {
+      teamAPoints += buttonNumber;
+      emit(ACounterIncrementStat());
+    } else {
+      teamBPoints += buttonNumber;
+      emit(BCounterIncrementStat());
+    }
+
+    // void teamBIncrement({required int buttonNumber}) {
+    //   teamBPoints += buttonNumber;
+    //   emit(BCounterIncrementStat());
+    // }
+  }
 }
-
-
 /*
 input: Function
                 Cubit
